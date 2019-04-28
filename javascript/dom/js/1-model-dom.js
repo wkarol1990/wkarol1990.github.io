4951f4addeba3c2c835ddc6eedd05ee43ce75ef2
Viachaslau Karol <bntu2014@gmail.com>
2019-04-28 09:29:25 +0200

    1-model-dom

---
+++ javascript/dom/js/1-model-dom.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log (window);
+console.log (window.innerHeight);
+console.log (window.outerHeight);
+
+console.log (document);
+console.log (document.documentElement);
+console.log (document.head);
+console.log (document.body);
